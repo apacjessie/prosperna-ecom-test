695d70e3337bbf1995d27f37351a9fc4c3115456
apacjessie <apacjessie@gmail.com>
2024-01-22 11:50:08 +0800

    Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import Header from "./components/element/Header";
+
 const App = () => {
   return (
     <div className="bg-red-500">
+      <Header />
       <h1>Hello world</h1>
     </div>
   );
